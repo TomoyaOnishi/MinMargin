4a73febe0de2efc6aa6eea0885b706318a21bd25
TomoyaOnishi <tomoyaonishi@icloud.com>
2018-08-27 16:17:00 +0900

    added.

---
+++ MinMargin/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "HelloHelloHelloHelloHelloHello"
+    }
+
+}
+
